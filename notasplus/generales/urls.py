4bf93632e932de9984ab840cca0fc23dabdba6bf
ivansoza <ivansz123@gmail.com>
2025-06-23 21:05:55 -0600

    kn m

--- notasplus/generales/urls.py
+++ notasplus/generales/urls.py
@@ -2,5 +2,14 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='home'),
+    path('',                         views.IndexView.as_view(),        name='home'),
+    # Materias...
+    path('materias/crear/',          views.MateriaCreateView.as_view(), name='materia_create'),
+    path('materias/',                views.MateriaListView.as_view(),   name='materia_list'),
+    path('materias/<int:pk>/',       views.MateriaDetailView.as_view(), name='materia_detail'),
+    # Alumnos
+    path('alumnos/crear/',           views.AlumnoCreateView.as_view(),  name='alumno_create'),
+    # Equipos...
+    path('equipos/crear/',           views.EquipoCreateView.as_view(),  name='equipo_create'),
+    path('equipos/',                 views.EquipoListView.as_view(),    name='equipo_list'),
 ]
--- notasplus/generales/views.py
+++ notasplus/generales/views.py
@@ -1,7 +1,55 @@
-from django.shortcuts import render
-from django.contrib.auth.decorators import login_required
+# views.py
+from django.urls import reverse_lazy
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.views.generic import TemplateView, CreateView, ListView, DetailView
+from equipos.models import Alumno, Materia, Equipo
+from django.db.models import Sum
+
+class IndexView(LoginRequiredMixin, TemplateView):
+    template_name = 'generales/index.html'
+
+    def get_context_data(self, **kwargs):
+        ctx = super().get_context_data(**kwargs)
+        ctx['materias'] = Materia.objects.all()
+        # Anotamos a cada alumno la suma de puntos de sus equipos
+        ctx['alumnos'] = (
+            Alumno.objects
+                  .annotate(total_points=Sum('equipos__puntos'))
+                  .prefetch_related('equipos')
+        )
+        return ctx
+class AlumnoCreateView(LoginRequiredMixin, CreateView):
+    model = Alumno
+    fields = ['apellido_paterno', 'apellido_materno', 'nombre', 'matricula', 'año_de_generacion']
+    template_name = 'generales/alumno_form.html'
+    success_url = reverse_lazy('home')
+
+class MateriaCreateView(LoginRequiredMixin, CreateView):
+    model = Materia
+    fields = ['nombre', 'codigo']
+    template_name = 'generales/materia_form.html'
+    success_url = reverse_lazy('home')
 
 
-@login_required
-def index(request):
-    return render(request, 'generales/index.html')
+class MateriaListView(LoginRequiredMixin, ListView):
+    model = Materia
+    context_object_name = 'materias'
+    template_name = 'generales/materia_list.html'
+
+
+class EquipoCreateView(LoginRequiredMixin, CreateView):
+    model = Equipo
+    fields = ['nombre', 'alumnos', 'materias']
+    template_name = 'generales/equipo_form.html'
+    success_url = reverse_lazy('home')
+
+
+class EquipoListView(LoginRequiredMixin, ListView):
+    model = Equipo
+    context_object_name = 'equipos'
+    template_name = 'generales/equipo_list.html'
+
+class MateriaDetailView(LoginRequiredMixin, DetailView):
+    model = Materia
+    context_object_name = 'materia'
+    template_name = 'generales/materia_detail.html'
