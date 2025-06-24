6251244ea78ba006eefa5b72fcd88a088e223754
ivansoza <94820666+ivansoza@users.noreply.github.com>
2025-06-23 19:18:23 -0600

    Add login functionality and enforce authentication

--- notasplus/notasplus/urls.py
+++ notasplus/notasplus/urls.py
@@ -16,8 +16,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('generales.urls')),
+    path('', auth_views.LoginView.as_view(template_name='generales/login.html'), name='login'),
+    path('login/', auth_views.LoginView.as_view(template_name='generales/login.html')),  # optional explicit login path
+    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+    path('home/', include('generales.urls')),
 ]
