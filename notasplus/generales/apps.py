4433aab6cabc9d1d702c3f38055c0edc6030914b
ivansoza <ivansz123@gmail.com>
2025-06-23 18:53:22 -0600

    ddd

--- notasplus/generales/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class GeneralesConfig(AppConfig):
-    default_auto_field = "django.db.models.BigAutoField"
-    name = "generales"
--- notasplus/notasplus/urls.py
+++ notasplus/notasplus/urls.py
@@ -2,7 +2,7 @@
 URL configuration for notasplus project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.2/topics/http/urls/
+    https://docs.djangoproject.com/en/5.0/topics/http/urls/
 Examples:
 Function views
     1. Add an import:  from my_app import views
@@ -14,14 +14,9 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-
 from django.contrib import admin
-from django.urls import path, include
-from django.contrib.auth import views as auth_views
+from django.urls import path
 
 urlpatterns = [
-    path('login/', auth_views.LoginView.as_view(template_name='registration/login.html'), name='login'),
-    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
-    path('', include('generales.urls')),
     path('admin/', admin.site.urls),
 ]
